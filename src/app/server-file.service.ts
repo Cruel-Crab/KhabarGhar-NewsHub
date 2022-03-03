980e03383174908a2f6278f3617e7e697c381ee0
Cruel-Crab <mohammadhirani-cmpn@atharvacoe.ac.in>
2022-03-03 18:44:29 +0530

    feat:changed api from newsapi to gnews

--- src/app/server-file.service.ts
+++ src/app/server-file.service.ts
@@ -10,9 +10,12 @@ export class ServerFileService {
   constructor(private _http: HttpClient) {
   }
 
-  allNewsURL = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=c889e51513e14bf8829fc6c164fcc451';
-  techNewsURL = 'https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=c889e51513e14bf8829fc6c164fcc451';
-  sportsNewsURL = 'https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=c889e51513e14bf8829fc6c164fcc451';
+  // allNewsURL = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=c889e51513e14bf8829fc6c164fcc451';
+  allNewsURL = 'https://gnews.io/api/v4/top-headlines?lang=en&country=in&token=1b22c075e8922ac2d6e3b9f6e898190d'
+  techNewsURL = 'https://gnews.io/api/v4/top-headlines?lang=en&country=in&topic=technology&token=1b22c075e8922ac2d6e3b9f6e898190d';
+  // techNewsURL = 'https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=c889e51513e14bf8829fc6c164fcc451';
+  sportsNewsURL = 'https://gnews.io/api/v4/top-headlines?lang=en&country=in&topic=sports&token=1b22c075e8922ac2d6e3b9f6e898190d'
+  // sportsNewsURL = 'https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=c889e51513e14bf8829fc6c164fcc451';
 
   getAllNews(): Observable<any> {
     return this._http.get(this.allNewsURL);
